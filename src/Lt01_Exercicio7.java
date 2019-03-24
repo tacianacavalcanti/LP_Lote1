241bf73ff186a84cd71d875bdfe101d0aadaeb36
Taciana Cavalcanti <taciacavalcantis@gmail.com>
2019-03-24 13:55:21 -0300

    pratica

--- src/Lt01_Exercicio7.java
+++ src/Lt01_Exercicio7.java
@@ -9,6 +9,15 @@ import javax.swing.JOptionPane;
 
 public class Lt01_Exercicio7 {
     public static void main(String args[]){
-    
+      /*Declaração de variaveis*/
+        int comp, larg, alt, vol;
+      /**Receber dados **/   
+
+        comp = Integer.parseInt(JOptionPane.showInputDialog("Informe comprimento do paralelepipedo"));
+        larg = Integer.parseInt(JOptionPane.showInputDialog("Informe a largura do paralelepipedo"));
+        alt = Integer.parseInt(JOptionPane.showInputDialog("Informe a altura do paralelepipedo"));
+
+        vol = comp * alt * larg;
+        JOptionPane.showMessageDialog(null,"O volume do paralelepipedo é " + vol);
     }
 }
